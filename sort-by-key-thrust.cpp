b0b64513bb8f2e03da81213584d6140ace6431de
Denis Demidov <ddemidov@ksu.ru>
2013-11-20 15:02:15 +0400

    Account for the possible holes in the set of keys.

--- sort-by-key-thrust.cpp
+++ sort-by-key-thrust.cpp
@@ -24,15 +24,16 @@ int main() {
     std::cout << ctx << std::endl;
 
     const size_t n = 1 << 24;
+    const size_t m = 100;
 
     // Generate key-value pairs on host. Keys are integers in [0,99] range.
     std::default_random_engine             rng(0);
-    std::uniform_int_distribution<int>     irnd(0, 99);
+    std::uniform_int_distribution<int>     irnd(0, m - 1);
     std::uniform_real_distribution<double> drnd(0.0, 1.0);
 
     std::vector<int>    key(n);
     std::vector<double> val(n);
-    std::vector<double> sum(100, 0.0);
+    std::vector<double> sum(m, 0.0);
 
     std::generate(key.begin(), key.end(), [&]() { return irnd(rng); });
     std::generate(val.begin(), val.end(), [&]() { return drnd(rng); });
@@ -50,39 +51,42 @@ int main() {
     vex::vector<double> v(ctx, val);
 
     // These will hold the reduced results:
-    vex::vector<int>    p(ctx, 100);
-    vex::vector<double> s(ctx, 100);
+    vex::vector<int>    p(ctx, m);
+    vex::vector<double> t(ctx, m);
+    vex::vector<double> s(ctx, m);
     prof.toc("Transfer");
 
     // Extract raw pointers for thrust.
     int    *k_begin = k(0).raw_ptr();
     int    *p_begin = p(0).raw_ptr();
     double *v_begin = v(0).raw_ptr();
-    double *s_begin = s(0).raw_ptr();
+    double *t_begin = t(0).raw_ptr();
 
     int    *p_end;
-    double *s_end;
+    double *t_end;
 
-    prof.tic_cl("GPU");
+    prof.tic_cl("Thrust");
     // Sort key-value pairs on the GPU:
     thrust_sort_by_key(k_begin, k_begin + n, v_begin);
 
     // Reduce values on the GPU:
-    std::tie(p_end, s_end) = thrust_reduce_by_key(
-            k_begin, k_begin + n, v_begin, p_begin, s_begin);
-    prof.toc("GPU");
+    std::tie(p_end, t_end) = thrust_reduce_by_key(
+            k_begin, k_begin + n, v_begin, p_begin, t_begin);
 
-    size_t n_keys = p_end - p_begin;
+    s = 0;
+    vex::permutation(
+            vex::vector<int>(ctx.queue(0), p(0), p_end - p_begin)
+            )(s) = t;
+    prof.toc("Thrust");
 
     prof.tic_cl("Transfer");
-    vex::copy(p.begin(), p.begin() + n_keys, key.begin());
-    vex::copy(s.begin(), s.begin() + n_keys, val.begin());
+    vex::copy(s.begin(), s.end(), val.begin());
     prof.toc("Transfer");
 
     // Check that we got correct results:
     double delta = 0;
-    for(size_t i = 0; i < n_keys; ++i)
-        delta += fabs(val[i] - sum[key[i]]);
+    for(size_t i = 0; i < m; ++i)
+        delta += fabs(val[i] - sum[i]);
 
     std::cout
         << "delta = " << delta << std::endl
